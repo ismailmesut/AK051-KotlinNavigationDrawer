e97f43a91fae35652ade877fd2c3ef4dd9ad9c18
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-20 15:37:25 +0300

    updated: coding process

--- app/src/main/java/com/ismailmesutmujde/kotlinnavigationdrawer/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinnavigationdrawer/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.ismailmesutmujde.kotlinnavigationdrawer
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.ismailmesutmujde.kotlinnavigationdrawer.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
 
+        // Navigation component ile Navigation Drawer bağlantısı
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(bindingMain.navigationView, navHostFragment.navController)
+
+        bindingMain.toolbar.title = "TITLE"
+        val toggle = ActionBarDrawerToggle(this, bindingMain.drawer, bindingMain.toolbar,0,0)
+        bindingMain.drawer.addDrawerListener(toggle)
+        toggle.syncState()
     }
 }
